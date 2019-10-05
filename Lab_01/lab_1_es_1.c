e5d21c4dc4d8e7e895b0ec2752a76dafb6c07031
Andrea De Masi <andreademasi80@gmail.com>
2019-10-05 12:26:30 +0200

    Lab 1 es 1

--- Lab_01/lab_1_es_1.c
+++ Lab_01/lab_1_es_1.c
@@ -1,31 +1,48 @@
+//
+// Created by andrea on 02/10/19.
+//
+
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    int campionato[20][20], giornata, i = 0, j = 0, righe, colonne;
+
+int main() {
+    int campionato[20][20], giornata, i = 0, j = 0, righe = 0, colonne = 0, capolista = 0;
     char c;
-    FILE* f;
+    FILE *f;
 
-    if((f = fopen("campionato.txt", "r")) == NULL){
+    if ((f = fopen("/home/andrea/apa/Lab_01/campionato.txt", "r")) == NULL) {
         printf("Errore file");
         return 1;
     }
 
-    while(fscanf(f,"%d%c", &giornata, &c) != EOF){
-        if(c == '\n'){
-            campionato[i][j] = giornata;
+    while (fscanf(f, "%d%c", &giornata, &c) != EOF) {
+        if (c != ' ') {
+            campionato[i][j] = giornata + campionato[i][j - 1];
             i++;
-            righe = i + 1;
-            colonne = j + 1;
             j = 0;
-        }
-        else{
+        } else if (j == 0) {
             campionato[i][j] = giornata;
+            j++;
+        } else {
+            campionato[i][j] = giornata + campionato[i][j - 1];
             j++;
         }
     }
 
     fclose(f);
+
+    colonne = j;
+    righe = i + 1;
+
+    for (j = 0; j < colonne; j++) {
+        for (i = 0; i < righe; i++) {
+            if (campionato[i][j] > campionato[capolista][j]) {
+                capolista = i;
+            }
+        }
+        printf("La capolista della giornata %d è la squadra numero %d\n", j + 1, capolista + 1);
+        capolista = 0;
+    }
+
     return 0;
 }
