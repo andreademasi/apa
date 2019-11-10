3ae08159b20b255b38a158f7e6ad674fcf54db48
Andrea De Masi <andreademasi80@gmail.com>
2019-11-10 11:50:48 +0100

    Fix #2

--- Lab_04/lab_4_es_2.c
+++ Lab_04/lab_4_es_2.c
@@ -6,47 +6,40 @@
 
 int majority(int *a, int N);
 
-struct m { // m = elemento maggioritario
-    int n;
-    int occurrencies;
-};
-
-struct m majorityR(int *a, int l, int r);
+int majorityR(int *a, int l, int r);
 
 int main() {
-    int V[8] = {0, 1, 0, 2, 3, 4, 0, 5};
+    int V[10] = {1, 1, 1, 2, 2, 2, 2, 2, 2, 1};
 
-    printf("%d", majority(V, 8));
+    printf("%d", majority(V, 10));
 }
 
 int majority(int *a, int N) {
     int l = 0, r = N - 1, i, j = 0;
-    struct m result = majorityR(a, l, r);
+    int result = majorityR(a, l, r);
     for (i = 0; i < N; i++) {
-        if (a[i] == result.n)
+        if (a[i] == result)
             j++;
         if (j > N / 2)
-            return result.n;
+            return result;
     }
     return -1;
 }
 
-struct m majorityR(int *a, int l, int r) {
-    int q = (l + r) / 2;
+int majorityR(int *a, int l, int r) {
+    int q = (l + r) / 2, i, occurencies = 0;
     if (r <= l) {
-        struct m n = {a[l], 1};
-        return n;
+        return a[l];
     }
-    struct m ml = majorityR(a, l, q); // maggioritario a sinistra
-    struct m mr = majorityR(a, q + 1, r); // maggioritario a destra
-    if (ml.n == mr.n) { // se il maggioritario è lo stesso sommo le occorrenze
-        ml.occurrencies += mr.occurrencies;
-        return ml;
-    } else if (ml.occurrencies > mr.occurrencies) //altrimenti seleziono il nuovo maggioritario
-        return ml;
-    else if (mr.occurrencies > ml.occurrencies)
-        return mr;
-    ml.n = -1; // se non c'è un maggioritario nel sottovettore
-    ml.occurrencies = 0;
-    return ml;
+    int ml = majorityR(a, l, q); // maggioritario a sinistra
+
+    int mr = majorityR(a, q + 1, r); // maggioritario a destra
+
+    for (i = l; i < r; i++) {
+        if (a[i] == ml)
+            occurencies++;
+        if (occurencies > ((r - l) / 2))
+            return ml;
+    }
+    return mr;
 }
